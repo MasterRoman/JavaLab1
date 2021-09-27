1b274bd238f2689b51a3b8856c2cdfaa3cee5caa
Roman Kozko <romanka2001@mail.ru>
2021-09-27 23:20:43 +0300

    add tasks with comparations

--- src/book/Book.java
+++ src/book/Book.java
@@ -1,19 +1,48 @@
 package book;
 
+import java.util.Comparator;
 import java.util.Objects;
 
-public class Book {
+public class Book implements Comparable<Book> {
     private String title;
     private String author;
     private int price;
+    private int isbn;
     private static int edition;
+
+    private static int count = 0;
 
     public Book(String title,String author,int price) {
         this.title = title;
         this.author = author;
         this.price = price;
+        this.isbn = ++count;
     }
 
+    public int compareTo(Book p){
+        if(isbn == p.getIsbn())
+            return 0;
+        else if(isbn > p.getIsbn())
+            return 1;
+        else
+            return -1;
+    }
+
+    public int getIsbn(){
+        return isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getPrice() {
+        return price;
+    }
 
     public String toString(){
         return "Book:" +
@@ -35,5 +64,33 @@ public class Book {
 
     protected Object clone() {
         return new Book(this.title,this.author,this.price);
+    }
+}
+
+class BookTitleComparator implements Comparator<Book>{
+
+    public int compare(Book a, Book b){
+        return a.getTitle().compareTo(b.getTitle());
+    }
+}
+
+class BookAuthorComparator implements Comparator<Book>{
+
+    public int compare(Book a, Book b){
+        return a.getAuthor().compareTo(b.getAuthor());
+    }
+
+}
+
+
+class BookPriceComparator implements Comparator<Book>{
+
+    public int compare(Book a, Book b){
+        if(a.getPrice() > b.getPrice())
+            return 1;
+        else if(a.getPrice() < b.getPrice())
+            return -1;
+        else
+            return 0;
     }
 }
--- src/book/Main.java
+++ src/book/Main.java
@@ -1,8 +1,43 @@
 package book;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        var book1 = new Book("1","1",1);
+        var book2 = new Book("5","5",5);
+        var book3 = new Book("2","5",1);
+        var book4 = new Book("1","3",1);
+        ArrayList<Book> books = new ArrayList<Book>();
+        books.add(book1);
+        books.add(book2);
+        books.add(book3);
+        books.add(book4);
+
+        Comparator<Book> compTitleAuthor = new BookTitleComparator().thenComparing(new BookAuthorComparator());
+        Comparator<Book> compAuthorTitle = new BookAuthorComparator().thenComparing(new BookTitleComparator());
+        Comparator<Book> compTitleAuthorPrice = new BookTitleComparator().thenComparing(new BookAuthorComparator())
+                                                                         .thenComparing(new BookPriceComparator());
+        books.sort(compTitleAuthor);
+
+        System.out.println("compTitleAuthor");
+        for (Book book:books) {
+            System.out.println(book.toString());
+        }
+
+        books.sort(compAuthorTitle);
+        System.out.println("compAuthorTitle");
+        for (Book book:books) {
+            System.out.println(book.toString());
+        }
+
+        books.sort(compTitleAuthorPrice);
+        System.out.println("compTitleAuthorPrice");
+        for (Book book:books) {
+            System.out.println(book.toString());
+        }
 
     }
 }
